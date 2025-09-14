4a0d0847255ed4f1bc03ad77c5c19d0ef3e4a56f
SevdaPDev <sevdapanahzadeh015@gmail.com>
2025-09-14 13:10:46 +0330

    Delet Token Button Created

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ import addTask from "./components/addTask.js";
 import renderTasks from "./components/Render.js";
 import Login from "./login.js";
 
-Login();
 export const renderAfter = () => {
   const token = localStorage.getItem("token");
-
   if (token) {
     document.getElementById("mainPage").innerHTML = `<h1 style="
-        color: #ff6ba3;
-        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-          'Lucida Sans', Arial, sans-serif;
-        font-size: 4rem;
+    color: #ff6ba3;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
+    font-size: 4rem;
         margin-bottom: 20px;
         font-weight: bold;
       ">
@@ -21,14 +19,30 @@ export const renderAfter = () => {
       <div class="bakhshinput">
         <input class="input" id="taskInput" type="text" placeholder="Add a task" />
         <button class="button" id="addBtn" >Add Task</button>
+        <button id="logouBtn">delet token</button>
       </div>
       <ul class="ul" id="taskList"></ul>
     </div>`;
     const addBtn = document.getElementById("addBtn");
-
-    // اضافه کردن Event Listener برای دکمه
     addBtn.addEventListener("click", () => addTask());
     renderTasks();
+    document.getElementById("logouBtn").addEventListener("click", () => {
+      localStorage.removeItem("token");
+      alert("توکن پاک شد");
+      renderAfter();
+    });
+  } else {
+    document.getElementById(
+      "mainPage"
+    ).innerHTML = `<form class="loginForm" id="loginForm">
+      <p class="p">login</p>
+      <input class="username" type="text" id="username" placeholder="نام کاربری" required>
+      <input type="password" id="password" placeholder="رمز عبور" required>
+      <input type="email" id="email" placeholder="ایمیل" required>
+      <button class="submit" type="submit">sign in</button>
+      <p class="error" id="errorMessage"></p>
+    </form>`;
+    Login();
   }
 };
 renderAfter();
